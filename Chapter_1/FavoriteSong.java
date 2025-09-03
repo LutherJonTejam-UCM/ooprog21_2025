7e1490051e13eff5cf53c935e69dd6c9da0ab5ca
LutherJonTejam-UCM <luthertejam23@gmail.com>
2025-09-03 03:02:13 -0700

    Update FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,8 +1,9 @@
 public class FavoriteSong {
     public static void main(String[] args) {
-        System.out.println("I want you to know how deep my love goes");
-        System.out.println("I ain't gotta a lot of time, no speech, just follow");
-        System.out.println("Some tricks in and up my sleeve at the magic show");
-        System.out.println("Got your body on top of me, all around, in and out you go");
+        System.out.println("He said, One day, you'll leave this world behind");
+        System.out.println("So live a life you will remember");
+        System.out.println("My father told me when I was just a child");
+        System.out.println("These are the nights that never die");
+        System.out.println("My father told me");
     }
 }
