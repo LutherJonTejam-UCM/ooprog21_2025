b8aa2236e925acbec0cb8d4b16acfa3612c26a45
LutherJonTejam-UCM <luthertejam23@gmail.com>
2025-09-15 09:00:15 +0800

    Update CompanyInfo.java

--- Chapter_3/CompanyInfo.java
+++ Chapter_3/CompanyInfo.java
@@ -1,1 +1,14 @@
+public class CompanyInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Smart Electronics");
+        displayHours();
+    }
+
+    public static void displayHours() {
+        System.out.println("Monday - Friday   8 AM to 6 PM");
+        System.out.println("Saturday:  8 AM to noon");
+        System.out.println("Sunday:    Closed");
+    }
+}
 
