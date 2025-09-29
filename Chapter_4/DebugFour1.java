c2abf38ca02c64e048140f9263930254f484cb57
LutherJonTejam-UCM <luthertejam23@gmail.com>
2025-09-29 08:31:47 +0800

    Update DebugFour1.java

--- Chapter_4/DebugFour1.java
+++ Chapter_4/DebugFour1.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+
+public class DebugFour1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+        int radius = scanner.nextInt();
+
+        DebugCircle circle = new DebugCircle(radius);
+
+        System.out.println("Circle with radius " + circle.getRadius());
+        System.out.println("Diameter: " + circle.getDiameter());
+        System.out.println("Area: " + circle.getArea());
+
+        scanner.close();
+    }
+}
 
