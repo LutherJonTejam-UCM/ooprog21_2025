efcfa75b8755d5c4c94db69415ab2ae4a5c60eef
LutherJonTejam-UCM <luthertejam23@gmail.com>
2025-09-29 08:39:06 +0800

    Update DebugBox.java

--- Chapter_4/DebugBox.java
+++ Chapter_4/DebugBox.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class DebugBox {
 
     private int width;
@@ -25,23 +23,4 @@ public class DebugBox {
     public int getVolume() {
         return length * width * height;
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the width of the box: ");
-        int width = scanner.nextInt();
-        System.out.print("Enter the length of the box: ");
-        int length = scanner.nextInt();
-        System.out.print("Enter the height of the box: ");
-        int height = scanner.nextInt();
-
-        DebugBox box = new DebugBox(width, length, height);
-
-        box.showData();
-        System.out.println("Volume: " + box.getVolume());
-      
-        scanner.close();
-    }
 }
-
