c21e7f3cb72df8ef06344355079ae06e83d8cb6d
LutherJonTejam-UCM <luthertejam23@gmail.com>
2025-09-29 08:35:11 +0800

    Update DebugPen.java

--- Chapter_4/DebugPen.java
+++ Chapter_4/DebugPen.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class DebugPen {
     private String color;
     private String point;
@@ -21,22 +19,4 @@ public class DebugPen {
     public String getPoint() {
         return point;
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter pen color: ");
-        String inputColor = scanner.nextLine();
-
-        System.out.print("Enter pen point: ");
-        String inputPoint = scanner.nextLine();
-
-        DebugPen pen = new DebugPen(inputColor, inputPoint);
-
-        System.out.println("Pen color: " + pen.getColor());
-        System.out.println("Pen point: " + pen.getPoint());
-
-        scanner.close();
-    }
 }
-
