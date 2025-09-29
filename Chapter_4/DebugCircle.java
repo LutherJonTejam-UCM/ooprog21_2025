347125fc28368f4a3154b94e63c146257b8a244d
LutherJonTejam-UCM <luthertejam23@gmail.com>
2025-09-29 08:14:36 +0800

    Update DebugCircle.java

--- Chapter_4/DebugCircle.java
+++ Chapter_4/DebugCircle.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
 public class DebugCircle {
-
     private int radius;
     private final double PI = 3.14159;
     private double area;
-
+    
     public DebugCircle(int r) {
         radius = r;
         area = PI * r * r; 
@@ -18,23 +15,7 @@ public class DebugCircle {
     public int getDiameter() {
         return 2 * radius; 
     }
-
+    
     public double getArea() {
         return area; 
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the radius of the circle: ");
-        int radius = scanner.nextInt();
-
-        DebugCircle circle = new DebugCircle(radius);
-        
-        System.out.println("Circle with radius " + circle.getRadius());
-        System.out.println("Diameter: " + circle.getDiameter());
-        System.out.println("Area: " + circle.getArea());
-
-        scanner.close();
-    }
-}
-
