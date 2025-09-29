4e8bdf981d2fbe74cf0a7da57be63701d3115df4
LutherJonTejam-UCM <luthertejam23@gmail.com>
2025-09-29 08:35:44 +0800

    Update DebugFour2.java

--- Chapter_4/DebugFour2.java
+++ Chapter_4/DebugFour2.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class DebugFour2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter pen color: ");
+        String inputColor = scanner.nextLine();
+
+        System.out.print("Enter pen point: ");
+        String inputPoint = scanner.nextLine();
+
+        DebugPen pen = new DebugPen(inputColor, inputPoint);
+
+        System.out.println("Pen color: " + pen.getColor());
+        System.out.println("Pen point: " + pen.getPoint());
+
+        scanner.close();
+    }
+}
 
